77de5e4f8df6ece712f427df1e78300c179523f9
shvepsy <shvepsy@users.noreply.github.com>
2015-05-25 07:28:59 +0300

    Create http.pl

---
+++ http.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use LWP::UserAgent;
+use HTTP::Cookies;
+require HTTP::Request;
+my $site = shift;
+
+#$print("$var1 $var2 $var3\n");
+#on what site
+my $pr = (print"Site: $site\n");
+#my $site=(<STDIN>);
+
+#init uagent
+my $ua = LWP::UserAgent->new;
+$ua->agent("ChromeFrome/0.1");
+
+#Go request
+my $req = HTTP::Request->new(GET => "http://$site");
+#$req->content_type('text/html');
+
+my $res = $ua->request($req);
+
+#check response
+if ($res->is_success) {
+print $res->content;
+}
+else {
+print $res->status_line, "\n";
+}
+;
