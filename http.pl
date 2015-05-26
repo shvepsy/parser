43f431762383ebfed5e70ea708e62f99a8cc7dd7
Shvepsy <exr2@mail.ru>
2015-05-26 03:36:37 +0300

    add in sub

--- http.pl
+++ http.pl
@@ -4,36 +4,37 @@ use warnings;
 use LWP::UserAgent;
 use HTTP::Cookies;
 require HTTP::Request;
-my $site = shift;
 
-if (defined $site == 0) {
-  print "Enter site URL: ";
-  $site = (<STDIN>);
-};
+our $site = shift;
+sub parse {
+  if (defined $site == 0) {
+    print "Enter site URL: ";
+    $site = (<STDIN>);
+  };
 
-#on what site
-my $pr = (print"Site: $site\n");
+  #on what site
+  my $pr = (print"Site: $site\n");
+  #init uagent
+  my $ua = LWP::UserAgent->new;
+  $ua->agent("ChromeFrome/0.1");
 
-#init uagent
-my $ua = LWP::UserAgent->new;
-$ua->agent("ChromeFrome/0.1");
+  #Go request
+  my $req = HTTP::Request->new(GET => "http://$site");
+  #$req->content_type('text/html');
 
-#Go request
-my $req = HTTP::Request->new(GET => "http://$site");
-#$req->content_type('text/html');
+  my $res = $ua->request($req);
 
-my $res = $ua->request($req);
-
-#check response
-if ($res->is_success) {
-my $rez = $res->content;
-#print "$rez\n\n";
-$rez =~ s|><|>\n<|g ;
-print "$rez";
-}
+  #check response
+  if ($res->is_success) {
+    my $rez = $res->content;
+    #print "$rez\n\n";
+    $rez =~ s|><|>\n<|g ;
+    print "$rez";
+  }
 else {
 print $res->status_line, "\n";
 }
-;
+;};
+parse;
 #windows win+R wait
 my $wait = (<STDIN>);
