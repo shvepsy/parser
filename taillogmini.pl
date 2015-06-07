4aaf04a4c5d06428a5e5b811dd88ab3bb19def6b
ShvepsyWork <eXr2@mail.ru>
2015-06-07 15:16:39 +0300

    After first test

--- taillogmini.pl
+++ taillogmini.pl
@@ -5,11 +5,6 @@ use File::Tail;
 my $ct = 0;
 my $border = 90; #
 my %hash;
-my @toban;
-#my %hs;
-my $count;
-my $txt = "";
-my $pr = "";
 my $lflow = File::Tail->new(name      => "/var/log/nginx/access.host.log",
                       interval      => 0.1,
                       maxinterval   => 0.5,
@@ -26,17 +21,20 @@ while (defined(my $line= $lflow->read)) {
    },
    );
    $ct++;
-   if ($ct >= 500) {
+   if ($ct >= 1500) {
      $ct = 0;
      foreach my $key (keys %hash) {
-       print $hash{$key}->{domain}."\t";
-       print $hash{$key}->{req}." \n";
-       if ($hash{$key}->{domain} eq "pornocaldo.com" || $hash{$key}->{domain} eq "www.pornocaldo.com"  ) { #
-         if ( $hash{$key}->{req} =~ m/GET \/.*.xml\?\S+ HTTP.*/  ) {
-           exit 0;
+       #print $hash{$key}->{domain}."\t";
+       #print $hash{$key}->{req}." \n";
+      if ($hash{$key}->{domain} =~ m/\S+ornocaldo\.com/  ) {
+#        if ( $hash{$key}->{req} =~ m/GET \/.*\..*\?\S+ HTTP.*/  ) {
+         if ( $hash{$key}->{req} =~ m/GET \/.*\?.*\S+ HTTP.*/  ) {
+           #exit 0;
            my $banip = $hash{$key}->{ip};
-           print "Banned: $banip";
-           #system ("/usr/sbin/ipset add test $banip -exist timeout 36000");
+           print "Banned: $banip\t".$hash{$key}->{req}."\n";
+           system ("/usr/sbin/ipset add test $banip -exist timeout 36000");
+
+
          }
        }
        #$txt = $txt.$hash{$key}->{domain}." ".$hash{$key}->{ip}." ".$hash{$key}->{md}."\n";
