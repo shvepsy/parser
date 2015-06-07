a67ef4bfda27d351fe4745aa1cef1d3d43d67753
ShvepsyWork <eXr2@mail.ru>
2015-06-07 15:19:53 +0300

    Shorting

--- taillogmini.pl
+++ taillogmini.pl
@@ -3,7 +3,6 @@ use strict;
 use warnings;
 use File::Tail;
 my $ct = 0;
-my $border = 90; #
 my %hash;
 my $lflow = File::Tail->new(name      => "/var/log/nginx/access.host.log",
                       interval      => 0.1,
@@ -24,22 +23,14 @@ while (defined(my $line= $lflow->read)) {
    if ($ct >= 1500) {
      $ct = 0;
      foreach my $key (keys %hash) {
-       #print $hash{$key}->{domain}."\t";
-       #print $hash{$key}->{req}." \n";
       if ($hash{$key}->{domain} =~ m/\S+ornocaldo\.com/  ) {
-#        if ( $hash{$key}->{req} =~ m/GET \/.*\..*\?\S+ HTTP.*/  ) {
          if ( $hash{$key}->{req} =~ m/GET \/.*\?.*\S+ HTTP.*/  ) {
            #exit 0;
            my $banip = $hash{$key}->{ip};
            print "Banned: $banip\t".$hash{$key}->{req}."\n";
            system ("/usr/sbin/ipset add test $banip -exist timeout 36000");
-
-
+           }
          }
        }
-       #$txt = $txt.$hash{$key}->{domain}." ".$hash{$key}->{ip}." ".$hash{$key}->{md}."\n";
-     }
-
     }
-    #exit 0;
  };
