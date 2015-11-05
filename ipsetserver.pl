fecec356f1785f8538ccddd74c050475ac6dc6e0
Shvepsy Lpt <eXr2@mail.ru>
2015-11-05 20:23:08 +0300

    update

--- ipsetserver.pl
+++ ipsetserver.pl
@@ -6,7 +6,7 @@ use Socket;
 # use port 4444 as default
 my $port = shift || 4444;
 my $proto = getprotobyname('tcp');
-my $server = "10.10.100.61";
+my $server = "192.168.1.36";
 
 # create a socket
 socket(SOCKET, PF_INET, SOCK_STREAM, $proto)
@@ -34,28 +34,28 @@ while ($client_addr = accept(NEW_SOCKET, SOCKET)) {
    # verification received data
    unless ($req_method eq "check"||$req_method eq "del") { print NEW_SOCKET "ERROR: Invalid method\n"; close NEW_SOCKET; next; };
    unless ($req_ip=~/^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/ &&(($1<=255  && $2<=255 && $3<=255  &&$4<=255 ))) { print NEW_SOCKET "ERROR: Invalid IP\n"; close NEW_SOCKET; next; };
-
+   my $ipset[2] = "BAN";
    # execute
    if ($req_method eq "check" ) {
-      #my $cmd = "/home/firewall/firewall.sh ipset | egrep 'Name: BAN|$req_ip' | sed -e 's| timeout.*||g' -e 's|Name: ||g' | grep -P '^\d+' -B 1 | grep -v '^--' | head -n 2";
-      my @ipset = `sudo ipset -L | egrep "Name: BAN|$req_ip" | sed -e "s| timeout.*||g" -e "s|Name: ||g" | grep -P "^\d+" -B 1 | grep -v "^--" | head -n 2`;
-      #my @ipset = system($cmd);
-      print "$?";
-      print "@ipset[0] $ipset[1]";
-      print NEW_SOCKET "Not ban\n" if (!@ipset);
-      print NEW_SOCKET "$ipset[0] banned in $ipset[1] table\n"
+      my $cmd = "ipset -L | egrep \'Name: BAN|$req_ip\' | sed -e \'s| timeout.*||g\' -e \'s|Name: ||g\' | grep -P \'^\\\d\+\' -B 1 | grep -v \'^--\' | head -n 2";
+      my @ipset = `$cmd`;
+      chomp ($ipset[0],$ipset[1]);
+      if (!@ipset[1]) { print NEW_SOCKET "Not ban.\n"; close NEW_SOCKET; next };
+      print "$req_method $ipset[0] $ipset[1]\n";
+      print NEW_SOCKET "$ipset[1] banned in $ipset[0] table.\n"
     }
     elsif ($req_method eq "del" ) {
-      #my $cmd = "/home/firewall/firewall.sh ipset | egrep 'Name: BAN|$req_ip' | sed -e 's| timeout.*||g' -e 's|Name: ||g' | grep -P '^\d+' -B 1 | grep -v '^--' | head -n 2";
-      my $cmd = "sudo ipset -L | egrep 'Name: BAN|$req_ip' | sed -e 's| timeout.*||g' -e 's|Name: ||g' | grep -P '^\d+' -B 1 | grep -v '^--' | head -n 2";
-      my @ipset = system($cmd);
+      my $cmd = "ipset -L | egrep \'Name: BAN|$req_ip\' | sed -e \'s| timeout.*||g\' -e \'s|Name: ||g\' | grep -P \'^\\\d\+\' -B 1 | grep -v \'^--\' | head -n 2";
+      my @ipset = `$cmd`;
+      chomp ($ipset[0],$ipset[1]);
+      print "$req_method @ipset";
       #my $cmd = "/home/firewall/firewall.sh ipsetdel $ipset[0] $ipset[1]";
-      my $cmd = "sudo ipset del $ipset[0] $ipset[1]";
-      my $del = system($cmd);
-      print $del;
-      if (!$del) { print NEW_SOCKET "Unbanned!\n" } else { print NEW_SOCKET "Not banned\n" };
+      my $cmd = "ipset del $ipset[0] $ipset[1]";
+      my @del = `$cmd`;
+      #print "\"@del\"";
+      if (@del == "") { print NEW_SOCKET "Not banned\n" } else { print NEW_SOCKET "Unbanned!\n" };
     }
-    else  { print NEW_SOCKET "ERROR:Case\n" }
-
+    else  { print NEW_SOCKET "ERROR: Case\n" }
+    my (@ipset, $cmd, @del) = "0";
    close NEW_SOCKET;
 }
