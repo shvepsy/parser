286b42b506cea3e4ff321581d7d913724c25633d
ShvepsyWork <eXr2@mail.ru>
2015-11-06 12:32:44 +0300

    work!

--- ipsetserver.pl
+++ ipsetserver.pl
@@ -1,12 +1,13 @@
 #!/usr/bin/env perl
-
 use strict;
 use Socket;
+use POSIX qw(strftime);
 
 # use port 4444 as default
 my $port = shift || 4444;
 my $proto = getprotobyname('tcp');
-my $server = "192.168.1.36";
+my $server = "10.10.100.61";
+my $logpath = "/var/log/unban.log";
 
 # create a socket
 socket(SOCKET, PF_INET, SOCK_STREAM, $proto)
@@ -20,11 +21,15 @@ bind( SOCKET, pack_sockaddr_in($port, inet_aton($server)))
 
 listen(SOCKET, SOMAXCONN) or die "listen: $!";
 
-print "Server started on port $port\n";
+open (LOG, ">>$logpath") or die "Cannot open $logpath: $!";
+my $time = strftime "%e %b %H:%M:%S %Y", localtime;
+print LOG "$time\tServer started on port $port\n";
+LOG->flush();
 
 # accepting a connection
 my $client_addr;
 while ($client_addr = accept(NEW_SOCKET, SOCKET)) {
+
    # send them a message, close connection
    my ($clinet_port,$client_ipn) = sockaddr_in($client_addr);
    my $client_ip = inet_ntoa($client_ipn);
@@ -34,28 +39,38 @@ while ($client_addr = accept(NEW_SOCKET, SOCKET)) {
    # verification received data
    unless ($req_method eq "check"||$req_method eq "del") { print NEW_SOCKET "ERROR: Invalid method\n"; close NEW_SOCKET; next; };
    unless ($req_ip=~/^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/ &&(($1<=255  && $2<=255 && $3<=255  &&$4<=255 ))) { print NEW_SOCKET "ERROR: Invalid IP\n"; close NEW_SOCKET; next; };
-   my $ipset[2] = "BAN";
+
    # execute
    if ($req_method eq "check" ) {
-      my $cmd = "ipset -L | egrep \'Name: BAN|$req_ip\' | sed -e \'s| timeout.*||g\' -e \'s|Name: ||g\' | grep -P \'^\\\d\+\' -B 1 | grep -v \'^--\' | head -n 2";
-      my @ipset = `$cmd`;
-      chomp ($ipset[0],$ipset[1]);
-      if (!@ipset[1]) { print NEW_SOCKET "Not ban.\n"; close NEW_SOCKET; next };
-      print "$req_method $ipset[0] $ipset[1]\n";
-      print NEW_SOCKET "$ipset[1] banned in $ipset[0] table.\n"
+     my @ipset = Check($req_ip);
+     my $time = strftime "%e %b %H:%M:%S %Y", localtime;;
+     print LOG "$time - $client_ip - $req_method $ipset[0] $ipset[1]\n";
+     print NEW_SOCKET "$ipset[1] banned in $ipset[0] table.\n";
     }
     elsif ($req_method eq "del" ) {
-      my $cmd = "ipset -L | egrep \'Name: BAN|$req_ip\' | sed -e \'s| timeout.*||g\' -e \'s|Name: ||g\' | grep -P \'^\\\d\+\' -B 1 | grep -v \'^--\' | head -n 2";
-      my @ipset = `$cmd`;
-      chomp ($ipset[0],$ipset[1]);
-      print "$req_method @ipset";
-      #my $cmd = "/home/firewall/firewall.sh ipsetdel $ipset[0] $ipset[1]";
-      my $cmd = "ipset del $ipset[0] $ipset[1]";
-      my @del = `$cmd`;
-      #print "\"@del\"";
-      if (@del == "") { print NEW_SOCKET "Not banned\n" } else { print NEW_SOCKET "Unbanned!\n" };
+      my ($del,@ipset) = Delete($req_ip);
+      if (!$del) {my $time = strftime "%e %b %H:%M:%S %Y", localtime; print LOG "$time - $client_ip - $req_method   $ipset[0] $ipset[1]\n"; print NEW_SOCKET "Unbanned!\n";} else {};
     }
     else  { print NEW_SOCKET "ERROR: Case\n" }
-    my (@ipset, $cmd, @del) = "0";
    close NEW_SOCKET;
+   LOG->flush();
+}
+close LOG;
+
+#subs Check and Delete->Check
+sub Check {
+  my ($req_ip) = @_;
+  my $cmd = "ipset -L | egrep \'Name: BAN|$req_ip\' | sed -e \'s| timeout.*||g\' -e \'s|Name: ||g\' | grep -P \'^\\\d\+\' -B 1 | grep -v \'^--\' | head -n 2";
+  my @ipset = `$cmd`;
+  if (!$ipset[1] or $ipset[1] !~ /$req_ip\,/ ) { print NEW_SOCKET "Not banned.\n"; close NEW_SOCKET; next }
+  chomp ($ipset[0],$ipset[1]);
+  return @ipset;
+  }
+
+sub Delete {
+  my ($req_ip) = @_;
+  my (@ipset) = Check($req_ip);
+  my $cmd = "ipset del $ipset[0] $ipset[1]";
+  my $del = `$cmd`;
+  return $del, @ipset;
 }
